cdcb2b2227dc6e82e08fddc2426737a3d3ac72bd
moneycash69 <108938667+moneycash69@users.noreply.github.com>
2023-04-25 08:51:05 -0300

    mosca 25/4

--- Assets/Scripts/MoscaCollision.cs
+++ Assets/Scripts/MoscaCollision.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class MoscaCollision : MonoBehaviour
 {
     public Vector3 initialPosition;
+    public int lives;
+     
+    public TextMeshProUGUI textVidas;
+    public TextMeshProUGUI txtScore;
+    public int score = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+        lives = 3;
         initialPosition = transform.position;
+        textVidas.text = "Vidas: " + lives;
+        txtScore.text = score.ToString();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -18,10 +26,25 @@ public class MoscaCollision : MonoBehaviour
         {
             //Destroy(gameObject);
             transform.position = initialPosition;
-        }else if (collision.gameObject.name == "Target")
+            lives--;
+            textVidas.text = "Vidas: " + lives;
+            if (lives==0)
+            {
+                PlayerDeath();
+            }
+        }
+            else if (collision.gameObject.name == "Target")
         {
             Destroy(collision.gameObject);
+            score++;
+            txtScore.text = score.ToString();
         }
-    }
 
+    }
+    void PlayerDeath()
+    {
+        Destroy(gameObject);
+        Time.timeScale = 0;
+        
+    }
 }
